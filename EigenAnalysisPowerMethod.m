9ec53611b7625a50c3178b3a3277aed9f71c7241
melanielele <zhiyue.zhao@emory.edu>
2020-11-03 23:42:45 -0500

    Add Question2 code

--- EigenAnalysisPowerMethod.m
+++ EigenAnalysisPowerMethod.m
@@ -1,8 +1,19 @@
-function v = EigenAnalysisPowerMethod(A, v0, Itr)
+function [v,lambda] = EigenAnalysisPowerMethod(A, v0, Itr)
 
+%set the initial eigenvector
 v = v0(:);
+
 for k = 1 : Itr
     v = A * v;
+    y = A*v;
+    l=y'*v;
+    m=v'*v;
+    % here is the eigenvalue
+    lambda =l/m;
+    % approximate the next eigenvectvor
+    plot(k,lambda,'-ok');
+    hold on;
     v = v / sqrt(v' * v);
 end
 
+ 
--- Eigenvalue.m
+++ Eigenvalue.m
@@ -1,2 +1,7 @@
 A=[4 1;1 3];
 [V,d]=eig(A);
+
+% Using the power method
+v0=[1 1];
+Itr=20;
+[v,lambda]=EigenAnalysisPowerMethod(A, v0, Itr);
