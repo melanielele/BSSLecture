32e0099bf876cd9a3d64b0c489525e71ebca68fd
melanielele <zhiyue.zhao@emory.edu>
2020-11-04 20:07:37 -0500

    Update Power Method script to add figures

--- EigenAnalysisPowerMethod.m
+++ EigenAnalysisPowerMethod.m
@@ -2,7 +2,8 @@ function [v,lambda] = EigenAnalysisPowerMethod(A, v0, Itr)
 
 %set the initial eigenvector
 v = v0(:);
-
+f1 = figure;
+f2 = figure;
 for k = 1 : Itr
     v = A * v;
     y = A*v;
@@ -11,7 +12,14 @@ for k = 1 : Itr
     % here is the eigenvalue
     lambda =l/m;
     % approximate the next eigenvectvor
+    %figure(1)
+    set(0, 'CurrentFigure', f1)
     plot(k,lambda,'-ok');
+    title("Eigenvalues vs. iteration")
+    hold on;
+    set(0, 'CurrentFigure', f2)
+    %figure(2)
+    plot(v);
     hold on;
     v = v / sqrt(v' * v);
 end
